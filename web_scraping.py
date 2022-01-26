0c8a9c0d3b5f751688e2d30324e54a2447c583eb
Praneeth <spraneeth4@gmail.com>
2022-01-26 22:14:39 +0530

    updated sidecar mounts and webscraping code

--- web_scraping.py
+++ web_scraping.py
@@ -13,10 +13,14 @@ time.sleep(5)
 def get_results(input_zip):
     input_zip = str(input_zip)
     assert len(input_zip) == 5
-
+    opts = webdriver.ChromeOptions()
+    # opts.binary_location = os.environ.get('GOOGLE_CHROME_BIN', None)
+    opts.add_argument("--headless")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--no-sandbox")
     # put the path of the downloaded chrome driver executable here"
     # driver_path = r"C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Remote("http://selenium-container:4444/wd/hub",desired_capabilities=DesiredCapabilities.CHROME)
+    driver = webdriver.Remote("http://localhost:4444/wd/hub", options=opts)
     # driver = webdriver.Chrome(driver_path)
 
     # put the url that you want to navigate to"
@@ -33,7 +37,7 @@ def get_results(input_zip):
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "statTable")))
 
         fetch_from_table = lambda row_index, data_index: (
-            driver.find_element(by=By.NAME,value=f"//table[@class= 'statTable']//tbody//tr[{row_index}]/td[{data_index}]").text)
+            driver.find_element(by=By.XPATH,value=f"//table[@class= 'statTable']//tbody//tr[{row_index}]/td[{data_index}]").text)
 
         cities_list = fetch_from_table(6, 2).split("\n")
         state = fetch_from_table(3, 2)
@@ -48,10 +52,10 @@ def get_results(input_zip):
     return result
 
 # Sample execution to test the fuction
-
+# 23509,32120,53214,13583,32121
 # if __name__ == "__main__":
-#     for i in [14701,23509,32120,53214,13583,32121]:
-#         get_results(i)
+#     for i in [14701]:
+#         print(get_results(i))
 
 
 # print(list(map(lambda x: table_data.text.replace("\n"," "))
